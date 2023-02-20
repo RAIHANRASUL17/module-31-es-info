ec7a584435e7bd706eab20b43bfbb97833ba8863
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-20 16:55:09 +0800

    default added

---
+++ arrow-5.js
@@ -0,0 +1,9 @@
+/* --------------------step-1:------------*/
+function add(first, second){
+    const total= first + second;
+    return total;
+}
+
+// call function
+const forReturnBtn= add(10, 20);
+console.log('The total value is: ', forReturnBtn)
